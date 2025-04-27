4fd9d8cc3c8240fd6d4dc99151b0119884f9ba2c
SandeepH2706 <sandeep0.gowda@gmail.com>
2025-04-27 23:40:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,30 @@
-# from flask import Flask
-# from models import db
-# from routes import routes_bp
-# from config import Config
+from flask import Flask
+from models import db
+from routes import routes_bp
+from config import Config
 
 # def create_app():
-#     app = Flask(__name__)
-#     app.config.from_object(Config)
-#     db.init_app(app)
-#     app.register_blueprint(routes_bp)
-#     return app
-
-
-# # app = create_app()  # <-- Add this line
-
-# if __name__ == '__main__':
-#     app = create_app()
-#     app.run(debug=True)
-
-
-from flask import Flask
-
 app = Flask(__name__)
+app.config.from_object(Config)
+db.init_app(app)
+app.register_blueprint(routes_bp)
+return app
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+
+# app = create_app()  # <-- Add this line
 
 if __name__ == '__main__':
+    # app = create_app()
     app.run(debug=True)
+
+
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# @app.route("/")
+# def hello():
+#     return "Hello, World!"
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
