41713871ae12e66b392a38b26e05b475a1675153
SandeepH2706 <sandeep0.gowda@gmail.com>
2025-04-28 00:11:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
