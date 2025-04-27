061c80f5ec2d26d6383031a8f77543bdaa3645c7
SandeepH2706 <sandeep0.gowda@gmail.com>
2025-04-27 19:46:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,6 +14,8 @@ def create_app():
     app.register_blueprint(routes_bp)
     return app
 
+app = create_app()  # <-- Add this line
+
 # if __name__ == '__main__':
 #     app = create_app()
 #     app.run(debug=False)
