a2012b3a97989d0d999e238f90043e3ab07ed2e2
SandeepH2706 <sandeep0.gowda@gmail.com>
2025-04-28 00:24:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,30 @@
+# from flask import Flask
+# from models import db
+# from routes import routes_bp
+# from config import Config
+
+# def app():
+#     app = Flask(__name__)
+#     app.config.from_object(Config)
+    
+#     db.init_app(app)
+#     with app.app_context():
+#         db.create_all()
+    
+#     app.register_blueprint(routes_bp)
+#     return app
+
+# if __name__ == '__main__':
+#     app = app()
+#     app.run(debug=True)
+
+
 from flask import Flask
 from models import db
 from routes import routes_bp
 from config import Config
 
-def app():
+def create_app():  # Renamed from app() to create_app()
     app = Flask(__name__)
     app.config.from_object(Config)
     
@@ -14,8 +35,7 @@ def app():
     app.register_blueprint(routes_bp)
     return app
 
+app = create_app()  # Instantiate the app for Gunicorn
+
 if __name__ == '__main__':
-    app = app()
     app.run(debug=True)
-
-
