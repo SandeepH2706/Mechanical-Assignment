ce4082ae577f5e6d180474a4c3ccd1080e8c60a0
SandeepH2706 <sandeep0.gowda@gmail.com>
2025-04-27 20:05:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,13 +6,10 @@ from config import Config
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-    
     db.init_app(app)
-    with app.app_context():
-        db.create_all()
-    
     app.register_blueprint(routes_bp)
     return app
+
 
 # app = create_app()  # <-- Add this line
 
