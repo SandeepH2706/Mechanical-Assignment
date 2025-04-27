48e2ca3c2732901e1f3c982a145e678691aa2860
SandeepH2706 <sandeep0.gowda@gmail.com>
2025-04-27 23:47:50 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
-from app import create_app
-# application = create_app() # Rename to 'application' (Gunicorn's default)
-# app = create_app()
-application = app
+# from app import create_app
+# # application = create_app() # Rename to 'application' (Gunicorn's default)
+# # app = create_app()
+# application = app
